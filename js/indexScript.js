af4800afaa454170ceec55071c67be1948644e62
Draaakos <orlando.andaur.c@gmail.com>
2017-10-07 15:37:35 -0300

    added modal for each image in index

---
+++ js/indexScript.js
@@ -0,0 +1,13 @@
+function modalImage(url) {
+    console.log(url);
+    let body=document.getElementsByTagName('body')[0].className="modal-active";
+    let newImage = document.createElement('img');
+    newImage.src = url;
+
+    document.getElementById('img-modal').appendChild(newImage);
+}
+
+function closeModal() {
+    let body=document.getElementsByTagName('body')[0].className='';
+    document.getElementById('img-modal').innerHTML='';
+}
